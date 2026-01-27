57809ff6baa5b423e0338eb50392e282d1791bc2
Gabriela-Moura <gabriela.m.souza10@aluno.senai.br>
2026-01-27 11:53:07 -0300

    20260127 - funcoes e operadores

---
+++ JAVASCRIPT/operadores_e_funcoes/script.js
@@ -0,0 +1,30 @@
+console.log("Olá, JavaScript!");
+
+let a= 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+let contador = 5;
+contador++;
+console.log(contador);
+
+let c = 10;
+let d = 7;
+let e = 14;
+
+console.log((c + d + e) / 3);
+console.log(e % c)
+
+let x= 10;
+let y= '10';
+
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+console.log(x !== y);
