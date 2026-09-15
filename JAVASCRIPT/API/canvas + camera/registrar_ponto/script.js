6231358c9a63885d868972cdd5260bad15203693
Gabriela-Moura <gabriela.m.souza10@aluno.senai.br>
2026-09-15 07:30:56 -0300

    Atividade registrar ponto

--- JAVASCRIPT/API/canvas + camera/registrar_ponto/script.js
+++ JAVASCRIPT/API/canvas + camera/registrar_ponto/script.js
@@ -1,12 +1,11 @@
-let confirmar = document.querySelector("#confirmar");
 let latitude = document.querySelector("#latitude");
 let longitude = document.querySelector("#longitude");
 let precisao = document.querySelector("#precisao");
 let video = document.querySelector('#camera');
 let canvas = document.querySelector("#canvas");
-let foto = document.querySelector("#foto");
+let confirmar = document.querySelector("#confirmar");
 
-//objeto.API.metodo
+//Localização -----------------------------------------------
 navigator.geolocation.getCurrentPosition(
     function (posicao) {
         latitude.innerHTML = "Latitude: " + posicao.coords.latitude;
@@ -22,7 +21,8 @@ navigator.geolocation.getCurrentPosition(
     },
 );
 
-//objeto.API.metodo
+//Câmera ----------------------------------------------------
+
 //navigator pois está no navegador, mediaDevices pois é um dispositivo de mídia, getUserMedia pois queremos obter a mídia do usuário
 navigator.mediaDevices.getUserMedia({ 
     video: true,
@@ -35,9 +35,14 @@ navigator.mediaDevices.getUserMedia({
     console.error("Erro ao acessar a câmera:", error);
 });
 
-//Evento de click, ao clicar no botão de confirmar, exibe um alerta de presença confirmada
-confirmar.addEventListener("click", function() {
-    //videoWidth e Heigth é do tamanho da resolução da câmera, o clientWidth e Height é do tamanho que eu defini
+//Botão confirmar -------------------------------------------
+
+//Evento de click, ao clicar no botão de confirmar, exibe uma mensagem de confirmação de presença em outra página
+
+console.log(confirmar);
+
+confirmar.addEventListener("click", function(event) {
+    //Definie o tamanho do canvas
     canvas.width = video.clientWidth;
     canvas.height = video.clientHeight;
 
@@ -51,5 +56,21 @@ confirmar.addEventListener("click", function() {
         canvas.height
     ),
 
-    foto.src = canvas.toDataURL("image/png");
+    // A variável "foto" recebe a imagem capturada pelo canvas em formato de string. Não usamos "foto.src" aqui porque "foto" não é um elemento <img>, mas sim os dados da imagem.
+
+    // Transforma a foto em uma imagem
+    foto = canvas.toDataURL("image/png");
+
+    console.log("Foto capturada!");
+    console.log(foto);
+
+    //"fotoPresenca" é o nome usado como chave no localStorage para identificar e armazenar a foto da presença. Assim, podemos recuperar essa mesma foto na página "presenca_confirmada.html".
+    
+    // Salva a foto no navegador
+    localStorage.setItem("fotoPresenca", foto);
+
+    console.log("Foto salva no localStorage")
+
+    // Vai para a página de confirmação
+    window.location.href = "presenca_confirmada.html";
 });
